7a800918edccf61f7d15edcfdc9c49f1a7ecf7e1
cuhaafke <126277273+cuhaafke@users.noreply.github.com>
2025-11-13 16:19:43 -0600

    Delete Get-PAM.ps1

--- Get-PAM.ps1
+++
@@ -1,6 +0,0 @@
-function global:Get-PAM{
-    param(
-        $Site = $Site
-    )  
-    Get-DatabaseAvailabilityGroup $Site* -Status -WarningAction SilentlyContinue | select Name, PrimaryActiveManager
-}
